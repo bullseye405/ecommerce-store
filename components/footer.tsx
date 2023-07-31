1b29cd95d067e627f39c2b64c4d1a15345920bda
Samir <bullseye405@gmail.com>
2023-07-31 17:29:45 +0545

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 StoreName, Inc. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/navbar-actions.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const NavbarActions = () => {
+  return <div className="ml-auto flex items-center gap-x-4"></div>;
+};
+
+export default NavbarActions;
